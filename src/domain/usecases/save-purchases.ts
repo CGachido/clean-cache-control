11bb9bdc2dad1f0a3ad3aa3b09b90e0c7d7ff90a
Cassius Souza <cassius.sanches@gmail.com>
2020-12-12 10:38:14 -0300

    Adding SavePurchases use case

---
+++ src/domain/usecases/save-purchases.ts
@@ -0,0 +1,17 @@
+/*
+ ** Arquivo do DOMAIN
+ ** Não coloca implementação de nada.
+ ** Somente interfaces e modelos (entities)
+ */
+
+export interface SavePurchases {
+  save: (purchases: Array<SavePurchases.Params>) => Promise<void>;
+}
+
+namespace SavePurchases {
+  export type Params = {
+    id: string;
+    date: Date;
+    value: number;
+  };
+}
