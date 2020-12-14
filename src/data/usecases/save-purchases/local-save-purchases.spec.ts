488635c95fa6201f6ae800b24d05b5516453197a
Cassius Souza <cassius.sanches@gmail.com>
2020-12-14 20:43:51 -0300

    Use await on async tests

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -28,12 +28,12 @@ describe('LocalSavePurchases', () => {
     expect(cacheStore.deleteKey).toBe('purchases');
   });
 
-  test('Should not insert new Cache if delete fails', () => {
+  test('Should not insert new Cache if delete fails', async () => {
     const { cacheStore, sut } = makeSut();
     cacheStore.simulateDeleteError();
     const promise = sut.save(mockPurchases());
     expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete]);
-    expect(promise).rejects.toThrow();
+    await expect(promise).rejects.toThrow();
   });
 
   test('Should insert new Cache if delete succeeds', async () => {
@@ -48,7 +48,7 @@ describe('LocalSavePurchases', () => {
     expect(cacheStore.insertValues).toEqual(purchases);
   });
 
-  test('Should throw if insert throws', () => {
+  test('Should throw if insert throws', async () => {
     const { cacheStore, sut } = makeSut();
     cacheStore.simulateInsertError();
     const promise = sut.save(mockPurchases());
@@ -56,6 +56,6 @@ describe('LocalSavePurchases', () => {
       CacheStoreSpy.Message.delete,
       CacheStoreSpy.Message.insert,
     ]);
-    expect(promise).rejects.toThrow();
+    await expect(promise).rejects.toThrow();
   });
 });
